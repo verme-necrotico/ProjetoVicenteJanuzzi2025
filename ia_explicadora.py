b11de17ec4f59affd3c62eb30e7ef86b46e2755f
verme-necrotico <hyok2712@gmail.com>
2025-08-09 14:13:56 -0300

    Update ia_explicadora.py

--- ia_explicadora.py
+++ ia_explicadora.py
@@ -465,10 +465,11 @@ elif st.session_state.etapa == 5:
         </div>
         """
 
-        components.html(html_code, height=400, width=1000)
+        components.html(html_code, height=500, width=1000)
 
 
     # fim
+
 
 
 
